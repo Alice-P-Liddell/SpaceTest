196fa91e814cd82a5ae4e6d22bf21e1783dfb741
Daehan Lee <alice.p.liddell0v0@gmail.com>
2019-11-20 18:21:37 +0900

    Master

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(0, Time.deltaTime * speed, 0);
+            // = Quaternion.Euler(0, Time.deltaTime * speed, 0);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SpaceTest
 for 2nd Test 2019-11-20
+
+This is Earth View.
+
+![Preview](./Preview/Preview.gif)
